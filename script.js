b173a8b15985e93ed0bf7cc798477d8737b9ead1
SandeepNandala <109787727+SandeepNandala@users.noreply.github.com>
2022-10-04 14:07:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
+//accessing the dom elements
+var searchInputEle = document.getElementById("super-hero-search");
+var favouriteEle = document.getElementById('favourites');
+var homeScreen = document.getElementById('home');
+var serchSubEle = document.getElementById('search-complete');
+
+//current time stamp 
+var ts = Date.now();
+
+//api public and private keys
+var public_key = '6eaf344d608cd6a64ae12f301e403823';
+var private_key = '30c982141433d2addba379ce978100fbead07e4d';
 
 
-var searchInputEle=document.getElementById("search-hero-search");
-var favouriteEle=document.getElementById('favourites');
-var removeFavrouteEle=document.getElementById('remove');
-var homeScreen=document.getElementById('home');
-var findHeroEle=document.getElementById('find-hero');
+let HerosListEle = "";//to add the Html content to dom
+let HerosObjList = {};//to store all the heros objects
 
+//in this function we are adding the all heros to super heros home page
+function addToHome(data) {
+    let heroObj = {};
 
-var ts= Date.now();
+    //iterating through all the objects in API data and adding it to DOM
+    for (let i in data) {
+        let HeroId = data[i]['id'];
+        heroObj['name'] = data[i]['name'];
+        heroObj['image'] = data[i]['thumbnail']['path'] + '.' + data[i]['thumbnail']['extension'];
+        heroObj['favourite'] = false;
 
-var public_key='6eaf344d608cd6a64ae12f301e403823';
-var private_key='30c982141433d2addba379ce978100fbead07e4d';
+        //adding all the objects to HerosObjList with their id as the property to use furthur 
+        HerosObjList[HeroId] = JSON.parse(JSON.stringify(heroObj));
 
-let HerosList="";
-let HerosArray={};
-function addToHome(data)
-{
-    let heroObj={};
-    for(let i in data)
-    {
-        // heroObj['id']=data[i]['id'];
-        let HeroId=data[i]['id'];
-        heroObj['name']=data[i]['name'];
-        heroObj['image']=data[i]['thumbnail']['path']+'.'+data[i]['thumbnail']['extension'];
-        heroObj['favourite']=false;
-
-        HerosArray[HeroId]=JSON.parse(JSON.stringify(heroObj));
-        HerosList+=`<div class="col">
+        //appending the heros divs to HerosListEle so we can add it to DOM.
+        HerosListEle += `<div class="col">
         <div class="card mt-3"  style="width: 18rem;">
             <img src=${heroObj['image']} id="hero-img" class="card-img-top" alt="...">
             <div class="card-body">
@@ -41,84 +45,67 @@ function addToHome(data)
             </div>
           </div>
         </div>`;
-
     }
 
-    document.getElementById('heros').innerHTML=HerosList;
+    //appending the heros to main super heros page DOM
+    document.getElementById('heros').innerHTML = HerosListEle;
 }
 
-async function hero()
-{
-    // var superhero = await fetch("https://gateway.marvel.com:443/v1/public/characters?ts=<time-stamp>&apikey=<public-key>&hash=<md5(ts+privateKey+publicKey)>");
-    var hashing=CryptoJS.MD5(ts+private_key+public_key);
-    
+
+//we are using this hero function/promise to fetch the api data from the  https://developer.marvel.com/docs 
+async function hero() {
+    var hashing = CryptoJS.MD5(ts + private_key + public_key);//using MD5 hasing algoritm
     let response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${public_key}&hash=${hashing}`);
-    let data=await response.json();
+    let data = await response.json();
     return data;
 }
 let ourHeros;
-hero().then((data)=>{
-    var count=0;
-    for(let i in data)
-    {
+
+hero().then((data) => {//if the promise fullfilled/resolved
+    var count = 0;
+    for (let i in data) {
         count++;
-        if(count==7)
-        {
+        if (count == 7) {
+            //using the 7 th count because the the actual api data resides in the results property
             addToHome(data[i]['results']);
-            ourHeros=data[i]['results'];
+            ourHeros = data[i]['results'];
         }
-          
     }
-       
-}).catch((error)=>{
+}).catch((error) => {//if the promise rejected
     console.log(error);
 });
 
-// ourHeros();
-
-function addToFavourites(event)
-{
-    
-    // console.log(HerosArray);
+//to add the hero to our favourites superhero list
+function addToFavourites(event) {
     event.preventDefault();
-    console.log("addToFavourites",event.target.getAttribute('data-id'));
-    if(event.target.getAttribute('data-id')!="" && event.target.id==='favourite')
-    {
-        let value=event.target.getAttribute('data-id')
-        HerosArray[value]['favourite']=true;
-        console.log(HerosArray[value]);
-        localStorage.setItem(value,HerosArray[value]);
+    if (event.target.getAttribute('data-id') != "" && event.target.id === 'favourite') {
+        let value = event.target.getAttribute('data-id')
+        //setting the favourite superhero flag to true
+        HerosObjList[value]['favourite'] = true;
+        //storing the hero in local storage so if we close the page and opens again in browser
+        //still we can see our favourite hero until unless you removed from favourite list 
+        localStorage.setItem(value, HerosObjList[value]);
         alert("added to favourites click on the favourites tab to see")
-        
     }
     event.stopPropagation();
-   
 }
 
-function findHero(event)
-{
+
+//this fuction will show the details of the specific super hero which user requested.
+function findHero(event) {
     event.preventDefault();
-    console.log("find-hero event.target.id",event.target.id);
-    console.log("event.target.id==='find-hero' && event.target.getAttribute('data-id')!= ",event.target.id==='find-hero' && event.target.getAttribute('data-id')!="")
-    if(event.target.id==='find-hero' && event.target.getAttribute('data-id')!=="" )
-    {
-        let value=event.target.getAttribute('data-id');
-        console.log("find-hero",value);
-        console.log("ourheros",ourHeros);
-        for(let i in ourHeros)
-        {
-            let herosPage="";
-            if(ourHeros[i]['id']==value)
-            {
-                let objHero={};
-                objHero['name']=ourHeros[i]['name'];
-                objHero['image']=ourHeros[i]['thumbnail']['path']+'.'+ourHeros[i]['thumbnail']['extension'];
-                objHero['comics']=ourHeros[i]['comics']['items'];
-                objHero['series']=ourHeros[i]['series']['items'];
-                
-                console.log('objhero',objHero)
-                // console.log('find me',ourHeros[i]);
-                herosPage=`
+    if (event.target.id === 'find-hero' && event.target.getAttribute('data-id') !== "") {
+        let value = event.target.getAttribute('data-id');
+        for (let i in ourHeros) {
+            let herosPage = "";
+            if (ourHeros[i]['id'] == value) {
+                let objHero = {};
+                objHero['name'] = ourHeros[i]['name'];
+                objHero['image'] = ourHeros[i]['thumbnail']['path'] + '.' + ourHeros[i]['thumbnail']['extension'];
+                objHero['comics'] = ourHeros[i]['comics']['items'];
+                objHero['series'] = ourHeros[i]['series']['items'];
+
+                herosPage = `
                 <div  class="col-md-6 mb-md-0 p-md-4">
                   <img id="hero-img-page" src="${objHero['image']}" class="w-100" alt="...">
                 </div>
@@ -130,46 +117,38 @@ function findHero(event)
                   <h3>Comics</h3>
                   <ul>
                   `
-                  for(let i in objHero['comics'])
-                  {
-                    herosPage+=`
+                //adding the comics to the super hero
+                for (let i in objHero['comics']) {
+                    herosPage += `
                     <li>${objHero['comics'][i]['name']}</li>`
-                  }
-
-                  herosPage+=` </ul><h3>Series</h3> <ul>`
-
-                  for(let i in objHero['series'])
-                  {
-                    herosPage+=`
+                }
+                // adding series of the super hero
+                herosPage += ` </ul><h3>Series</h3> <ul>`
+                for (let i in objHero['series']) {
+                    herosPage += `
                     <li>${objHero['series'][i]['name']}</li>  
-                   ` 
-                  }
-
-                 herosPage+=` </ul> </div>`
-                
-              document.getElementById('heros').innerHTML=herosPage;
+                   `
+                }
+                herosPage += ` </ul> </div>`
+                //appending to the super hero page DOM 
+                document.getElementById('heros').innerHTML = herosPage;
             }
-
         }
-
     }
     event.stopPropagation();
-  
+
 }
 
-function homeEle(event)
-{
-    let favouriteHeros="";
-    for(let i in  HerosArray)
-    {
-    
 
-        // if(HerosArray[i]['favourite']==true)
-        favouriteHeros+=` <div class="col">
+//if user clicks on the home button this function will shows the list of all super heros.
+function homeEle(event) {
+    let Heros = "";
+    for (let i in HerosObjList) {
+        Heros += ` <div class="col">
         <div data-id="${i}" class="card mt-3"  style="width: 18rem;">
-            <img src=${HerosArray[i]['image']} id="hero-img" class="card-img-top" alt="...">
+            <img src=${HerosObjList[i]['image']} id="hero-img" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 id="hero-name" class="card-title">${HerosArray[i]['name']}</h5>
+              <h5 id="hero-name" class="card-title">${HerosObjList[i]['name']}</h5>
               <p id="hero-details" class="card-text">MARVEL Studios superhero character. Usually appears in Marvel Comics and movies</p>
               <span>
               <a id="find-hero" data-id="${i}" href="" class="btn btn-primary">Find Me</a>
@@ -180,85 +159,71 @@ function homeEle(event)
               </div>
           </div>
         </div>`;
-        
-
     }
     event.stopPropagation();
-    document.getElementById('heros').innerHTML=favouriteHeros;
+    document.getElementById('heros').innerHTML = Heros;
 }
 
-function showFavourites(event)
-{
-    let favouriteHeros="";
-    for(let i in  HerosArray)
-    {
-    
 
-        // if(HerosArray[i]['favourite']==true)
-        if(localStorage.getItem(i)!=null)
-        {
-        favouriteHeros+=` <div class="col">
+//this function will shows the our favourite super heros page
+function showFavourites(event) {
+    let favouriteHeros = "";
+    for (let i in HerosObjList) {
+        //if the superhero id exists in local storage that means we added that super hero to our favourites
+        if (localStorage.getItem(i) != null) {
+            favouriteHeros += ` <div class="col">
         <div data-id="${i}" class="card mt-3"  style="width: 18rem;">
-            <img src=${HerosArray[i]['image']} id="hero-img" class="card-img-top" alt="...">
+            <img src=${HerosObjList[i]['image']} id="hero-img" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 id="hero-name" class="card-title">${HerosArray[i]['name']}</h5>
+              <h5 id="hero-name" class="card-title">${HerosObjList[i]['name']}</h5>
               <p id="hero-details" class="card-text">MARVEL Studios superhero character. Usually appears in Marvel Comics and movies</p>
               <a id="find-hero" data-id="${i}" href="" class="btn btn-primary">find me</a>
               <a id="remove" href="" data-id="${i}" class="btn btn-outline-danger">Remove</a>
-              
             </div>
           </div>
         </div>`;
         }
-
     }
     event.stopPropagation();
-    document.getElementById('heros').innerHTML=favouriteHeros;
+    //append the favourite superhero list to favourites DOM 
+    document.getElementById('heros').innerHTML = favouriteHeros;
 }
 
-function removeFavourites(event)
-{
+//this function will the super hero which is requested by the user from the favourites 
+function removeFavourites(event) {
     event.preventDefault();
-    console.log('event.target.id remove',event.target.id)
-    if(event.target.getAttribute('data-id')!="" && event.target.id==='remove')
-    {
-        let value=event.target.getAttribute('data-id');
-        HerosArray[value]['favourite']=false;
+    if (event.target.getAttribute('data-id') != "" && event.target.id === 'remove') {
+        let value = event.target.getAttribute('data-id');
+        //resetting the flag to false to remove from favourites 
+        HerosObjList[value]['favourite'] = false;
+        //deleting the super hero id from local storage we will not get the hero in favourites page again
         localStorage.removeItem(value);
-        let cardDiv=document.getElementsByClassName('card');
-        console.log(cardDiv);
-        for(let i=0;i<cardDiv.length;i++)
-        {
-            console.log(cardDiv[i].getAttribute('data-id'));
-            if(cardDiv[i].getAttribute('data-id')==value)
-            {
-                cardDiv[i].innerHTML='';
-                console.log('remove')
+        let cardDiv = document.getElementsByClassName('card');
+        for (let i = 0; i < cardDiv.length; i++) {
+            //once the hero removed from favourites then that hero should be removed from screen also 
+            if (cardDiv[i].getAttribute('data-id') == value) {
+                cardDiv[i].innerHTML = '';
                 break;
             }
         }
-        // alert('removed from favourites please click on the favourites tab again to see the changes');
     }
     event.stopPropagation();
 }
 
-function searchHero(event)
-{
-    let favouriteHeros="";
-    
-    for(let i in  HerosArray)
-    {
-    
-        let value=event.target.value;
-        let name=HerosArray[i]['name'].toLowerCase();
-        console.log('value',value,'name',name)
-        if(name.includes(value.toLowerCase())){
-        // if(HerosArray[i]['favourite']==true)
-        favouriteHeros+=` <div class="col">
+
+//this function will filter the super heros upon user search.
+function searchHero(event) {
+    let favouriteHeros = "";
+    for (let i in HerosObjList) {
+        let value = event.target.value;
+        let name = HerosObjList[i]['name'].toLowerCase();
+       //if there are any super heros with the searched name then it will reflect on page
+        if (name.includes(value.toLowerCase())) {
+            favouriteHeros += ` <div class="col">
         <div data-id="${i}" class="card mt-3"  style="width: 18rem;">
-            <img src=${HerosArray[i]['image']} id="hero-img" class="card-img-top" alt="...">
+            <img src=${HerosObjList[i]['image']} id="hero-img" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 id="hero-name" class="card-title">${HerosArray[i]['name']}</h5>
+              <h5 id="hero-name" class="card-title">${HerosObjList[i]['name']}</h5>
               <p id="hero-details" class="card-text">MARVEL Studios superhero character. Usually appears in Marvel Comics and movies</p>
               <span>
               <a id="find-hero" data-id="${i}" href="" class="btn btn-primary">Find Me</a>
@@ -269,27 +234,31 @@ function searchHero(event)
               </div>
           </div>
         </div>`;
-        
         }
     }
     event.stopPropagation();
-    if(favouriteHeros!="")
-     document.getElementById('heros').innerHTML=favouriteHeros;
-    else
-     document.getElementById('heros').innerHTML=`<h1 class="bg-light">No Results found</h1>`
+    if (favouriteHeros != "")
+        document.getElementById('heros').innerHTML = favouriteHeros;
+    else//if there no super heros with that name then it will show no results found
+        document.getElementById('heros').innerHTML = `<h1 class="bg-light">No Results found!!</h1>`
 }
 
-document.addEventListener('click',addToFavourites);
-
-favouriteEle.addEventListener('click',showFavourites);
-
-document.addEventListener('click',removeFavourites);
-
-homeScreen.addEventListener('click',homeEle);
-
-document.addEventListener('click',findHero);
-
-searchInputEle.addEventListener('keyup',searchHero);
+//to add the super hero to our favourites
+document.addEventListener('click', addToFavourites);
+//to show us the our favourite super heros 
+favouriteEle.addEventListener('click', showFavourites);
+//to remove the super hero from our favouries.
+document.addEventListener('click', removeFavourites);
+//to show us the home screen 
+homeScreen.addEventListener('click', homeEle);
+//to show us the details about the super hero
+document.addEventListener('click', findHero);
+//to filter the heros with search name.
+searchInputEle.addEventListener('keyup', searchHero);
+//to complete the search
+serchSubEle.addEventListener('click', () => {
+    searchInputEle.value = "";
+})
 
 
 
